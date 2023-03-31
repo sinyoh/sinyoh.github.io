1efce791774b313fd35713daaa502c42cac18f0c
sinyoh <81504197+sinyoh@users.noreply.github.com>
2023-03-31 08:04:04 +0700

    init

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+
+
+// workbox.routing.registerRoute(
+//     ({request}) => request.destination === 'image',
+//     new workbox.strategies.NetworkFirst()
+// );
+
+const _cacheName = 'my_cache';
+
+//cached file
+const _url = [
+  '/',
+  'styles.css',
+  '/images/about-header.jpg',
+  '/images/contact-image.jpg',
+  '/images/footer-background.png',
+  '/images/header-background.jpg',
+  '/images/logo.png',
+  'https://code.getmdl.io/1.3.0/material.grey-pink.min.css',
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(_cacheName).then(function (cache) {
+      console.log('tes');
+      return cache.addAll(_url);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (respose) {
+      return respose || fetch(event.request);
+    })
+  );
+});
